14e936ff09bc2a94cc8185eaeacbdbac8b3a8300
Tan12 <2064556850@qq.com>
2016-12-30 16:55:26 +0800

    web music player

--- HTML5_Audio_API/visualizer.js
+++ HTML5_Audio_API/visualizer.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/12/22 0022.
  */
 window.onload = function(){
-    visualizer.init();
+    new visualizer().init();
 };
 
 var visualizer = function () {
@@ -29,7 +29,7 @@ visualizer.prototype = {
         try {
             this.audioContext = new AudioContext();
         } catch(e) {
-            alert("啊哦，您的浏览器不支持 Web Audio API 哦！");
+            this.infoUpdateId("啊哦，您的浏览器不支持 Web Audio API 哦！", false);
             console.log(e);
         }
     },
@@ -42,7 +42,7 @@ visualizer.prototype = {
                 return false;
             }
             if(audioInput.files.length !== 0){ // 判断用户是否真的选择了文件，若取消了则文件长度为0
-                that.file = audioInput.file[0];
+                that.file = audioInput.files[0];
                 that.fileName = that.file.name;
                 if(that.status === 1){
                     that.forceStop = true;
@@ -53,16 +53,45 @@ visualizer.prototype = {
             that._start();
         };
         // 监听拖拽与释放事件
+        dropContainer.addEventListener('dragenter', function () {
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Drop it on the page', true);
+        }, false);
+        dropContainer.addEventListener('dragover', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.dataTranfer.dropEffect = 'copy';
+        }, false);
+        dropContainer.addEventListener('dragleave', function () {
+            document.getElementById('fileWrapper').style.opacity = 0.2;
+            that._updateInfo(that.info, false);
+        },false);
+        dropContainer.addEventListener('drop', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            if(that.audioContext === null){
+                return 0;
+            }
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Uploading', true);
+            that.file = e.dataTransfer.files[0];
+            if(that.status === 1){
+                document.getElementById('fielWrapper').style.opacity = 1;
+                that.forceStop = true;
+            }
+            that.fileName = that.file.name;
+            that._start();
+        }, false);
     },
     _start : function () {
         var that = this,
             file = this.file,
             fr = new FileReader();
-        fr.onload = function () { // 文件读取完调用此函数
+        fr.onload = function (e) { // 文件读取完调用此函数
             var fileResult = e.target.result,
                 audioContext = that.audioContext;
             if(audioContext === null){
-                return false;
+                return 0;
             }
             that._updateInfo('Decoding the audio', true);
             audioContext.decodeAudioData(fileResult, function (buffer) {
@@ -102,10 +131,64 @@ visualizer.prototype = {
         this.source = audioBufferSourceNode;
         audioBufferSourceNode.onended = function () {
             that._audioEnd(that);
-        }
+        };
         this._updateInfo('Playing ' + this.fileName, false);
         this.info = 'Playing ' + this.fileName;
         document.getElementById('fileWrapper').style.opacity = 0.2;
+        this._drawSpectrum(analyser);
+    },
+    _drawSpectrum : function(analyser){
+        var that = this,
+            canvas = document.getElementById('canvas'),
+            cwidth = canvas.width,
+            cheight = canvas.height - 2,
+            meterWidth = 10,
+            gap = 2,
+            capHeight = 2,
+            capStyle = '#fff',
+            meterNum = 800 / (10 + 2),
+            capYPositionArray = [];
+        ctx = canvas.getContext('2d');
+        gradient = ctx.createLinearGradient(0, 0, 0, 300);
+        gradient.addColorStop(1, '#0f0');
+        gradient.addColorStop(0.5, '#ff0');
+        gradient.addColorStop(0, '#f00');
+        var drawMeter = function () {
+            var array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            if(that.status === 0){
+                for(var i = array.length - 1; i >= 0; i--){
+                    array[i] = 0;
+                }
+                allCapsReachBottom = true;
+                for(var i = capYPositionArray.length - 1; i >= 0; i--){
+                    allCapsReachBottom = allCapsReachBottom && (capYPositionArray[i] === 0);
+                }
+                if(allCapsReachBottom){
+                    cancelAnimationFrame(that.animationId);
+                    return false;
+                }
+            }
+            var step = Math.round(array.length / meterNum);
+            ctx.clearRect(0, 0, cwidth, cheight);
+            for(var i = 0; i < meterNum; i++){
+                var value = array[i * step];
+                if(capYPositionArray.length < Math.round(meterNum)){
+                    capYPositionArray.push(value);
+                }
+                ctx.fillStyle = capStyle;
+                if(value < capYPositionArray[i]){
+                    ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+                }else {
+                    ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                    capYPositionArray[i] = value;
+                }
+                ctx.fillStyle = gradient;
+                ctx.fillRect(i * 12, cheight - value + capHeight, meterWidth, cheight);
+            }
+            that.animatedId = requestAnimationFrame(drawMeter);
+        };
+        this.animationId = requestAnimationFrame(drawMeter);
     },
     _updateInfo : function (text, processing) {
         var infoBar = document.getElementById('info'),
@@ -113,8 +196,8 @@ visualizer.prototype = {
             i = 0,
             that = this;
         infoBar.innerHTML = text + dots.substring(0, i++);
-        if(that.infoUpdateId !== null){
-            clearTimeout(that.infoUpdateId);
+        if(this.infoUpdateId !== null){
+            clearTimeout(this.infoUpdateId);
         }
         if(processing){
             var animateDot = function () {
@@ -126,5 +209,18 @@ visualizer.prototype = {
             };
             this.infoUpdateId = setTimeout(animateDot, 250);
         }
+    },
+    _audioEnd : function (instance) {
+        if(this.forceStop){
+            this.forceStop = false;
+            this.status = 1;
+            return false;
+        }
+        this.status = 0;
+        var text = 'HTML5 Audio API showcase | An Audio Visualizer';
+        document.getElementById('fileWrapper').style.opacity = 1;
+        document.getElementById('info').innerHTML = text;
+        instance.info = text;
+        document.getElementById('uploadedFile').value = '';
     }
 };
